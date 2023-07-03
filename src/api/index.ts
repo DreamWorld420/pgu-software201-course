75cd8219d28d1251e77d4366582e3b882fb299f4
Kasra Bozorgmehr <dreamworld420@protonmailcom>
2023-07-03 13:40:19 +0330

    axiosInstance

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,4 +5,16 @@ const axiosInstance = axios.create({
 	baseURL: config.baseURLForAPI ?? "localhost:3000",
 });
 
+axiosInstance.interceptors.request.use((config) => {
+	const token = localStorage.getItem("token");
+
+	if (!token) {
+		return config;
+	}
+
+	config.headers.Authorization = `Bearer ${token}`;
+
+	return config;
+});
+
 export default axiosInstance;
